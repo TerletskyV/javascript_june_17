c823afe9bf8cbfe01816a8e9831678a2ad4fa4c6
Vadim Terletsky <desfero364@gmail.com>
2017-07-13 23:26:20 +0300

    Created functions folder and done first homework

--- array/js/script.js
+++ array/js/script.js
@@ -1,4 +1,5 @@
-var arr = [];
+var arr    = [],
+	arrRev = [];
 function rand(min, max){
 	return Math.floor(Math.random() * (max - min)) + min;
 }
@@ -8,23 +9,71 @@ var min = 0,
 /*
 	Заполнение массива случайными числами
 */
-for(i = 0; i < 30; i++){
-	arr[i] = rand(min, max);
+ for(var i = 0; i < 10; i++){
+ 	arr[i] = [];
+	for(var j = 0; j < 10; j++ ){
+		arr[i][j] = rand(min, max);
+	}
 }
-console.log(arr);
+console.table(arr);
+
+// for(var i = 0; i < arr.length; i++){
+// 	arrRev[i] = [];
+// 	for(var j = 0; j < arr.length; j++){
+// 		arrRev[i][j] = arr[arr.length - 1 - i][arr.length - 1 - j];
+// 	}
+// }
+// console.table(arrRev);
+
+// var topSide = arr[0];
+// var rightSide = [];
+// for(var i = 1; i < arr.length - 1; i++){
+// 	rightSide[i - 1] = arr[i][arr.length - 1];
+// }
+// var bottomSide = arr[arr.length - 1].reverse();
+// var leftSide = [];
+// for(var i = 1; i < arr.length - 1; i++){
+// 	leftSide[i - 1] = arr[i][0];
+// }
+// leftSide.reverse();
+// var perimArr = topSide.concat(rightSide, bottomSide, leftSide);
+// console.log(perimArr);
+
+// Triangle
+
+// var triTop = arr[0];
+// var triRigth = [];
+// for(var i = 1; i < arr.length; i++){
+// 	triRigth[i - 1] = arr[i][arr.length - 1];
+// }
+// var triDiag = [];
+// for(var i = 1; i < arr.length - 1; i++){
+// 	triDiag[i - 1] = arr[i][i];
+// }
+// triDiag.reverse();
+// var arrTriangle = triRigth.concat(triDiag, triTop);
+// console.log(arrTriangle);
+
+
 /*
 	Поиск индекса числа numIndex в массиве
 */
-for(var i = 0; i < arr.length; i++){
-	if(arr[i] == numIndex){
-	console.log("Индексы числа " + numIndex + " : " + i);
-	}
-}
-/*
-	Поиск чётных элементов в массиве
-*/
-for(var i = 0; i < arr.length; i++){
-	if(arr[i] % 2 == 0){
-		console.log("Индексы чётных элементов в массиве: " + i);
-	}
-}
+// for(var i = 0; i < arr.length; i++){
+// 	if(arr[i] == numIndex){
+// 	console.log("Индексы числа " + numIndex + " : " + i);
+// 	}
+// }
+
+// 	Поиск чётных элементов в массиве
+
+// for(var i = 0; i < arr.length; i++){
+// 	if(arr[i] % 2 == 0){
+// 		console.log("Индексы чётных элементов в массиве: " + i);
+// 	}
+// }
+
+//Spiral
+
+var lngSidesBorders  = [0, arr.length - 1],
+	shrtSidesBorders = [1, arr.length - 2],
+	resArr = [];
